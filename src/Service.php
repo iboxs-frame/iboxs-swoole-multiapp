b569f700ad7925c6d20f15d44748f7f78e1e3006
itlattice <zqu1016@qq.com>
2025-12-04 14:22:05 +0800

    perf:新版本

--- src/Service.php
+++ src/Service.php
@@ -1,32 +1,18 @@
 <?php
-// +----------------------------------------------------------------------
-// | iboxsPHP [ WE CAN DO IT JUST iboxs IT ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2018 http://iboxsphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
-namespace iboxs\app;
 
-use iboxs\Service as BaseService;
+namespace IBoxs\SwooleMultiApp;
 
-class Service extends BaseService
+class Service extends \iboxs\Service
 {
+
+    /**
+     * 服务启动
+     *
+     * @return void
+     */
     public function boot()
     {
-        $this->app->event->listen('HttpRun', function () {
-            $this->app->middleware->add(MultiApp::class);
-        });
-
-        $this->commands([
-            'build' => command\Build::class,
-            'clear' => command\Clear::class,
-        ]);
-
-        $this->app->bind([
-            'iboxs\route\Url' => Url::class,
-        ]);
+        $this->app->bind('iboxs\swoole\Http', Http::class);
     }
+
 }
